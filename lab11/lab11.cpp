72ccdc59f7305bce568be57e6c7ff5651d2028c6
Ruby Aguilar <raguilar008@student.butte.edu>
2017-08-25 01:57:41 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,17 @@
+Begin with a board with the numbers 1-25
+Have a score for the game that starts at 10 when the game begins
+When program begins it is to pick a number between 1-25 as the x-value
+
+(loop)
+
+user/player is to then click on the number on the board they are guessing with their mouse
+if the number the user chooses is wrong the score will decrease by one point
+if the users guess has a value greater than the compter's x value  the screen will display "higher"
+if the users guess has a value less than the computer's x value the screen will display "lower"
+
+if the score reaches a value of 0 the correct answer will be highlighted and the screen will display "Sorry, you lose. :("
+
+if the number the user chooses is correct the screen will display "Correct!" 
+
+
+
