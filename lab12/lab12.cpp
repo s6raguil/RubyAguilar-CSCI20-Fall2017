3b7dad528379810151f54ba90d8076f8e0c3b4b6
Ruby Aguilar <raguilar008@student.butte.edu>
2017-08-30 01:54:03 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,21 @@
+/* Ruby Aguilar
+August 29, 2017
+Lab 1.2 ASCII art */
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "     ⩓________⩓     " << endl;   
+    cout << "    ⎛   ◉   ◉  ⎞    " << endl;       
+    cout << "    ⎜   (⦿⦿)   ⎜    " << endl;
+    cout << "    ⎝__________⎠    " << endl;
+    cout << "   ⎛            ⎞   " << endl;
+    cout << "   ⎟            ⎟   " << endl;
+    cout << "   ⎟            ⎟∝  " << endl;
+    cout << "   ⎝__☗______☗__⎠   " << endl;
+    
+}
+
+
